b610b357884c1dbed3328c51ec21eecfa95a2517
m-88888888 <mochi.h8888@gmail.com>
2022-12-18 17:14:04 +0900

    finish 1-1

--- 1-Iterator/BookShelf.java
+++ 1-Iterator/BookShelf.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
+
 public class BookShelf implements Aggregate{
-    private Book[] books;
+    private ArrayList<Book> books;
     private int last = 0;
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf() {
+        this.books = new ArrayList<Book>();
     }
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
+        this.books.add(book);
         last++;
     }
     public int getLength() {
